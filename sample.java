c53743689bd862fd4af205ff4593a995e9177910
TejashriRavichandra <Tejashriravichandra@gmail.com>
2019-07-16 17:43:05 +0530

    code update

--- sample.java
+++ sample.java
@@ -2,8 +2,8 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+      int num1 = 5, num2 = 15, sum; 
+      sum = num1 + num2;  //addition
 
       System.out.println("Sum of these numbers: "+sum);
    }
